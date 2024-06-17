9da5aba935df63f8be05987c50f2c7ea4491b11d
Vividh AntPC <vividhkothari@outlook.com>
2024-06-17 09:28:44 +0530

    MainActivity: Modified layout by adding spacer and adding alignments to the top greeting. Also changed the greeting from simple text to current date.

--- app/src/main/java/com/app/routineturboa/MainActivity.kt
+++ app/src/main/java/com/app/routineturboa/MainActivity.kt
@@ -3,34 +3,40 @@ package com.app.routineturboa
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.core.view.WindowCompat
+import androidx.compose.ui.unit.dp
 import com.app.routineturboa.services.MSALAuthManager
 import com.app.routineturboa.ui.MainScreen
 import com.app.routineturboa.ui.theme.RoutineTurboATheme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     private lateinit var msalAuthManager: MSALAuthManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         msalAuthManager = MSALAuthManager.getInstance(this)
 
         setContent {
             RoutineTurboATheme {
                 Surface {
-                    Column(
-                        modifier = Modifier.statusBarsPadding()
-                    ) {
+                    Column{
                         Greeting()
+                        Spacer(modifier = Modifier.height(2.dp)) // Optional spacer
                         MainScreen()
                     }
                 }
@@ -40,6 +46,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(){
-    Text(text = "Routine", style = MaterialTheme.typography.headlineLarge)
+fun Greeting() {
+    val currentDate = SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.getDefault()).format(Date())
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(text = currentDate, style = MaterialTheme.typography.headlineLarge)
+    }
 }
+
