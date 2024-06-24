49146410491eb98a0e48fec7a68c79656ba7e763
Vividh AntPC <vividhkothari@outlook.com>
2024-06-24 12:43:23 +0530

    Refactored functions, modified interface,

---
+++ app/src/main/java/com/app/routineturboa/services/downloadFromOneDrive.kt
@@ -0,0 +1,42 @@
+package com.app.routineturboa.services
+
+import android.content.Context
+import android.util.Log
+import com.app.routineturboa.data.local.DatabaseHelper
+import com.app.routineturboa.viewmodel.TaskViewModel
+import com.microsoft.identity.client.IAuthenticationResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+suspend fun downloadFromOneDrive(authResult: IAuthenticationResult, context: Context, taskViewModel: TaskViewModel) {
+    Log.d("MainScreen", "Downloading from OneDrive")
+    val authProvider = OneDriveManager.MsalAuthProvider(authResult)
+    val oneDriveManager = OneDriveManager(authProvider)
+
+    val files = withContext(Dispatchers.IO) {
+        oneDriveManager.listFiles()
+    }
+
+    val routineTurboDir = files.find { it.name == "RoutineTurbo" && it.folder != null }
+
+    routineTurboDir?.let { dir ->
+        val dirFiles = dir.id?.let { dirId ->
+            withContext(Dispatchers.IO) {
+                oneDriveManager.listFiles(dirId)
+            }
+        }
+
+        val dbFile = dirFiles?.find { it.name == "RoutineTurbo.db" }
+
+        dbFile?.let { driveItem ->
+            driveItem.id?.let { driveItemId ->
+                val localDbFile = context.getDatabasePath(DatabaseHelper.DATABASE_NAME)
+                withContext(Dispatchers.IO) {
+                    oneDriveManager.downloadFile(driveItemId, localDbFile)
+                }
+            }
+        }
+    }
+    taskViewModel.loadTasks()
+    Log.d("MainScreen", "Finished downloading from OneDrive")
+}
